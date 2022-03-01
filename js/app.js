1299f8520369778439c5cea55bbda587f51c74aa
pritom48 <pritamdas41148@gmail.com>
2022-03-01 10:07:16 +0600

    second commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("clicked")
+const searchPhone = () => {
+    //get search field result
+    const searchField = document.getElementById('input-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    searchField.value = '';
+
+    //load information from api
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+}
